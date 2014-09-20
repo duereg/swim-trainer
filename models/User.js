54d36877a4a074dc5ac6ba6f29d142effc835be7
Matt <duereg@hotmail.com>
2014-09-19 21:45:56 -0700

    remove everything but google and facebook

--- models/User.js
+++ models/User.js
@@ -7,11 +7,8 @@ var userSchema = new mongoose.Schema({
   password: String,
 
   facebook: String,
-  twitter: String,
   google: String,
-  github: String,
   instagram: String,
-  linkedin: String,
   tokens: Array,
 
   profile: {
