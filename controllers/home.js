6c65c66daa949570154f4ac02f74004f3aa9f8cd
Matt <duereg@hotmail.com>
2014-09-20 22:24:45 -0700

    add quotes

--- controllers/home.js
+++ controllers/home.js
@@ -3,8 +3,17 @@
  * Home page.
  */
 
+var quotes = require('../models/quotes');
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var quoteNum = getRandomInt(0, quotes.length);
+
 exports.index = function(req, res) {
   res.render('home', {
-    title: 'Home'
+    title: 'Home',
+    quote: quotes[quoteNum]
   });
 };
---
+++ models/quotes.js
@@ -0,0 +1,412 @@
+module.exports = [
+  {
+    "quote": "Never give up, never give in, and when the upper hand is ours, may we have the ability to handle the win with the dignity that we absorbed the loss.",
+    "author": "Doug Williams"
+  },
+  {
+    "quote": "Persistence can change failure into extraordinary achievement.",
+    "author": "Matt Biondi"
+  },
+  {
+    "quote": "It’s not the will to win that matters—everyone has that. It’s the will to prepare to win that matters.",
+    "author": "Bear Bryant"
+  },
+  {
+    "quote": "The difference between the impossible and the possible lies in a person's determination.",
+    "author": "Tommy Lasorda"
+  },
+  {
+    "quote": "Make sure your worst enemy doesn't live between your ears.",
+    "author": "Laird Hamilton"
+  },
+  {
+    "quote": "Champions keep playing until they get it right.",
+    "author": "Billie Jean King"
+  },
+  {
+    "quote": "If you fail to prepare, you're prepared to fail.",
+    "author": "Mark Spitz"
+  },
+  {
+    "quote": "A trophy carries dust. Memories last forever.",
+    "author": "Mary Lou Retton"
+  },
+  {
+    "quote": "The more difficult the victory, the greater the happiness in winning.",
+    "author": "Pele"
+  },
+  {
+    "quote": "One man can be a crucial ingredient on a team, but one man cannot make a team.",
+    "author": "Kareem Abdul-Jabbar"
+  },
+  {
+    "quote": "Leadership, like coaching, is fighting for the hearts and souls of men and getting them to believe in you.",
+    "author": "Eddie Robinson"
+  },
+  {
+    "quote": "You are never really playing an opponent. You are playing yourself, your own highest standards, and when you reach your limits, that is real joy.",
+    "author": "Arthur Ashe"
+  },
+  {
+    "quote": "Always make a total effort, even when the odds are against you.",
+    "author": "Arnold Palmer"
+  },
+  {
+    "quote": "You can motivate by fear, and you can motivate by reward. But both those methods are only temporary. The only lasting thing is self motivation.",
+    "author": "Homer Rice"
+  },
+  {
+    "quote": "Without self-discipline, success is impossible, period.",
+    "author": "Lou Holtz"
+  },
+  {
+    "quote": "Do you know what my favorite part of the game is? The opportunity to play.",
+    "author": "Mike Singletary"
+  },
+  {
+    "quote": "You find that you have peace of mind and can enjoy yourself, get more sleep, and rest when you know that it was a one hundred percent effort that you gave–win or lose.",
+    "author": "Gordie Howe"
+  },
+  {
+    "quote": "Show me a guy who's afraid to look bad, and I'll show you a guy you can beat every time.",
+    "author": "Lou Brock"
+  },
+  {
+    "quote": "If you have everything under control, you're not moving fast enough.",
+    "author": "Mario Andretti"
+  },
+  {
+    "quote": "Just keep going. Everybody gets better if they keep at it.",
+    "author": "Ted Williams"
+  },
+  {
+    "quote": "To uncover your true potential you must first find your own limits and then you have to have the courage to blow past them.",
+    "author": "Picabo Street"
+  },
+  {
+    "quote": "If you can believe it, the mind can achieve it.",
+    "author": "Ronnie Lott"
+  },
+  {
+    "quote": "Push yourself again and again. Don't give an inch until the final buzzer sounds.",
+    "author": "Larry Bird"
+  },
+  {
+    "quote": "What to do with a mistake: recognize it, admit it, learn from it, forget it.",
+    "author": "Dean Smith"
+  },
+  {
+    "quote": "I hated every minute of training, but I said, 'Don’t quit. Suffer now and live the rest of your life as a champion.",
+    "author": "Muhammad Ali"
+  },
+  {
+    "quote": "Your biggest opponent isn't the other guy. It's human nature.",
+    "author": "Bobby Knight"
+  },
+  {
+    "quote": "There are only two options regarding commitment. You're either IN or you're OUT. There is no such thing as life in-between.",
+    "author": "Pat Riley"
+  },
+  {
+    "quote": "Never let your head hang down. Never give up and sit down and grieve. Find another way.",
+    "author": "Satchel Paige"
+  },
+  {
+    "quote": "Do not let what you can not do interfere with what you can do.",
+    "author": "John Wooden"
+  },
+  {
+    "quote": "A champion is someone who gets up when he can't.",
+    "author": "Jack Dempsey"
+  },
+  {
+    "quote": "It ain't over till it's over.",
+    "author": "Yogi Berra"
+  },
+  {
+    "quote": "I've missed more than 9,000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
+    "author": "Michael Jordan"
+  },
+  {
+    "quote": "You're never a loser until you quit trying.",
+    "author": "Mike Ditka"
+  },
+  {
+    "quote": "You miss 100 percent of the shots you don't take.",
+    "author": "Wayne Gretzky"
+  },
+  {
+    "quote": "Never give up! Failure and rejection are only the first step to succeeding.",
+    "author": "Jim Valvano"
+  },
+  {
+    "quote": "How hard would you play, if you knew you couldn't play tomorrow?"
+  },
+  {
+    "quote": "Winning is habit. Unfortunately, so is losing.",
+    "author": "Vince Lombardi"
+  },
+  {
+    "quote": "A good hockey player plays where the puck is. A great hockey player plays where the puck is going to be.",
+    "author": "Wayne Gretzky"
+  },
+  {
+    "quote": "Success is where preparation and opportunity meet.",
+    "author": "Bobby Unser"
+  },
+  {
+    "quote": "I am building a fire, and everyday I train, I add more fuel. At just the right moment, I light the match.",
+    "author": "Mia Hamm"
+  },
+  {
+    "quote": "Nobody's a natural. You work hard to get good and then work to get better. It's hard to stay on top.",
+    "author": "Paul Coffey"
+  },
+  {
+    "quote": "Just be patient. Let the game come to you. Don't rush. Be quick, but don't hurry.",
+    "author": "Earl Monroe"
+  },
+  {
+    "quote": "There may be people that have more talent than you, but theres no excuse for anyone to work harder than you do.",
+    "author": "Derek Jeter"
+  },
+  {
+    "quote": "When you’ve got something to prove, there’s nothing greater than a challenge.",
+    "author": "Terry Bradshaw"
+  },
+  {
+    "quote": "If you can’t outplay them, outwork them.",
+    "author": "Ben Hogan"
+  },
+  {
+    "quote": "Continuous effort — not strength or intelligence — is the key to unlocking our potential.",
+    "author": "Liane Cardes"
+  },
+  {
+    "quote": "Good is not good when better is expected.",
+    "author": "Vin Scully"
+  },
+  {
+    "quote": "You were born to be a player. You were meant to be here. This moment is yours.",
+    "author": "Herb Brooks"
+  },
+  {
+    "quote": "hat you lack in talent can be made up with desire, hustle, and giving 110 percent all the time.",
+    "author": "Don Zimmer"
+  },
+  {
+    "quote": "How you respond to the challenge in the second half will determine what you become after the game, whether you are a winner or a loser.",
+    "author": "Lou Holtz"
+  },
+  {
+    "quote": "Sports serve society by providing vivid examples of excellence.",
+    "author": "George F Will"
+  },
+  {
+    "quote": "The principle is competing against yourself. It’s about self-improvement, about being better than you were the day before.",
+    "author": "Steve Young"
+  },
+  {
+    "quote": "The road to Easy Street goes through the sewer.",
+    "author": "John Madden"
+  },
+  {
+    "quote": "Nobody who ever gave his best regretted it.",
+    "author": "George Halas"
+  },
+  {
+    "quote": "Stubbornness usually is considered a negative; but I think that trait has been a positive for me.",
+    "author": "Cal Ripken Jr."
+  },
+  {
+    "quote": "You’ve got to take the initiative and play your game. In a decisive set, confidence is the difference.",
+    "author": "Chris Evert"
+  },
+  {
+    "quote": "When you win, say nothing, when you lose, say less.",
+    "author": "Paul Brown"
+  },
+  {
+    "quote": "The hardest skill to acquire in this sport is the one where you compete all out, give it all you have, and you are still getting beat no matter what you do. When you have the killer instinct to fight through that, it is very special.",
+    "author": "Eddie Reese"
+  },
+  {
+    "quote": "The mind is the limit. As long as the mind can envision the fact that you can do something, you can do it, as long as you really believe 100 percent.",
+    "author": "Arnold Schwarzenegger"
+  },
+  {
+    "quote": "When I go out there, I have no pity on my brother. I am out there to win.",
+    "author": "Joe Frazier"
+  },
+  {
+    "quote": "I never left the field saying I could have done more to get ready and that gives me piece of mind.",
+    "author": "Peyton Manning"
+  },
+  {
+    "quote": "Most talented players don’t always succeed. Some don’t even make the team. It’s more what’s inside.",
+    "author": "Brett Favre"
+  },
+  {
+    "quote": "You have to expect things of yourself before you can do them.",
+    "author": "Michael Jordan"
+  },
+  {
+    "quote": "Show me a guy who’s afraid to look bad, and I’ll show you a guy you can beat every time.",
+    "author": "Lou Brock"
+  },
+  {
+    "quote": "I didn’t believe in team motivation. I believe in getting a team prepared so it knows it will have the necessary confidence when it steps on the field and be prepared to play a good game.",
+    "author": "Tom Landry"
+  },
+  {
+    "quote": "If you train hard, you’ll not only be hard, you’ll be hard to beat.",
+    "author": "Herschel Walker"
+  },
+  {
+    "quote": "If you don’t have confidence, you’ll always find a way not to win.",
+    "author": "Carl Lewis"
+  },
+  {
+    "quote": "Obstacles don’t have to stop you. If you run into a wall, don’t turn around and give up. Figure out how to climb it, go through it, or work around it.",
+    "author": "Michael Jordan"
+  },
+  {
+    "quote": "Make each day your masterpiece.",
+    "author": "John Wooden"
+  },
+  {
+    "quote": "Excellence is the gradual result of always striving to do better.",
+    "author": "Pat Riley"
+  },
+  {
+    "quote": "Win If You Can, Lose If You Must, But NEVER QUIT!",
+    "author": "Cameron Trammell"
+  },
+  {
+    "quote": "Wisdom is always an overmatch for strength.",
+    "author": "Phil Jackson"
+  },
+  {
+    "quote": "You have to believe in yourself when no one else does. ",
+    "author": "Venus Williams"
+  },
+  {
+    "quote": "The highest compliment that you can pay me is to say that I work hard every day, that I never dog it.",
+    "author": "Wayne Gretzky"
+  },
+  {
+    "quote": "There are no shortcuts to any place worth going.",
+    "author": "Beverly Sills"
+  },
+  {
+    "quote": "You only ever grow as a human being if you're outside your comfort zone.",
+    "author": "Percy Cerutty"
+  },
+  {
+    "quote": "It is only through work and strife that either nation or individual moves on to greatness. The great man is always the man of mighty effort, and usually the man whom grinding need has trained to mighty effort.",
+    "author": "Theodore Roosevelt, in a speech about Grant, delivered at Galena, Illinois, April 27, 1900"
+  },
+  {
+    "quote": "The turning point in the process of growing up is when you discover the core of strength within you that survives all hurt.",
+    "author": "Max Lerner"
+  },
+  {
+    "quote": "The more I talk to athletes, the more convinced I become that the method of training is relatively unimportant. There are many ways to the top, and the training method you choose is just the one that suits you best. No, the important thing is the attitude of the athlete, the desire to get to the top.",
+    "author": "Herb Elliott"
+  },
+  {
+    "quote": "Restlessness and discontent is the first necessity of progress. Show me a thoroughly satisfied man and I will show you a failure.",
+    "author": "Thomas Alva Edison"
+  },
+  {
+    "quote": "Only those who risk going to far can possibly find out how far one can go.",
+    "author": "T.S. Eliot"
+  },
+  {
+    "quote": "Daring ideas are like chessmen moved forward; they may be beaten, but they may start a winning game. ",
+    "author": "Johann Wolfgang von Goethe"
+  },
+  {
+    "quote": "Man cannot discover new oceans unless he has the courage to lose sight of the shore. ",
+    "author": "Andre Gide"
+  },
+  {
+    "quote": "Inspiration cannot be willed, though it can be wooed.",
+    "author": "Anthony Storr"
+  },
+  {
+    "quote": "Problems are only opportunities in work clothes. ",
+    "author": "Henry J. Kaiser"
+  },
+  {
+    "quote": "Only he who can see the invisible can do the impossible. ",
+    "author": "Frank L. Gaines"
+  },
+  {
+    "quote": "Nothing will work unless you do.",
+    "author": "John Wooden"
+  },
+  {
+    "quote": "Everything you want is out there waiting for you to ask. Everything you want also wants you. But you have to take action to get it.",
+    "author": "Jack Canfield"
+  },
+  {
+    "quote": "The difference between the impossible and the possible lies in a person's determination.",
+    "author": "Tommy Lasorda "
+  },
+  {
+    "quote": "If you want to be the best, Work hardier than the rest. If you don't want to be the best, Just work like the rest.",
+    "author": "Robert Stinson"
+  },
+  {
+    "quote": "Hard training, easy combat; easy training, hard combat",
+    "author": "Marshal Suvorov, famous Russian General"
+  },
+  {
+    "quote": "The Truth is that Running Hurts. No one gets faster without meeting their personal pain barrier straight on. No amount of junk miles, fun runs or affirmations are going to get you over the hill at the five mile mark in a 10k. However, what will pull you through is solid prep with hard hill runs and interval work.",
+    "author": "Manciata's explanation of the Truth about Running"
+  },
+  {
+    "quote": "The pain of discipline is far less than the pain of regret",
+    "author": "Sarah Bombell, synchronized swimmer"
+  },
+  {
+    "quote": "It's a dream until you write it down, and then its a goal."
+  },
+  {
+    "quote": "Obstacles are those frightening things that become visible when we take our eyes off our goals.",
+    "author": "Henry Ford"
+  },
+  {
+    "quote": "You must take action now that will move you towards your goals. Develop a sense of urgency in your life. ",
+    "author": "Les Brown"
+  },
+  {
+    "quote": "Keep away from people who try to belittle your ambitions. Small people always do that, but the really great make you feel",
+    "author": "Mark Twain"
+  },
+  {
+    "quote": "When you cease to dream you cease to live. ",
+    "author": "Malcolm Forbes"
+  },
+  {
+    "quote": "We are not interested in the possibilities of defeat; They do not exist.",
+    "author": "A.J.Balfour"
+  },
+  {
+    "quote": "Man is not cut out for defeat. Man can be destroyed but not defeated.",
+    "author": "E. Hemingway"
+  },
+  {
+    "quote": "If you believe in yourself, have dedication and pride and never quit, you'll be a winner. The price of victory is high, but so are the rewards.",
+    "author": "Bear Bryant"
+  },
+  {
+    "quote": "Far better it is to dare mighty things, to win glorious triumphs, even though checkered by failure ... than to rank with those poor spirits who neither enjoy much nor suffer much, because they live in a grey twilight that knows not ",
+    "author": "Theodore Roosevelt"
+  },
+  {
+    "quote": "When it comes down to that moment - when it’s me against you, you know in your head whether you worked hard enough. You can try to lie to yourself. You can try to tell yourself that you put in the time. But you know — and so do I.",
+    "author": "J.J. Watt"
+  }
+];
